a500b3a20c6b701eaddb386136d67298be082bf3
yusakabe <82621508+yusakabe@users.noreply.github.com>
2022-11-28 22:59:07 +0900

    complete text commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,49 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
+import { History } from './pages/history'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import { useStateWithStorage } from './hooks/use_state_with_storage'
 
-const Main = (<h1>Markdown Editer</h1>)
+const GlobalStyle = createGlobalStyle`
+body * {
+  box-sizing: border-box;
+}
+`
 
-render(Main, document.getElementById('app'))
+const StorageKey = '/editor:text'
+
+const Main: React.FC = () => {
+  const [text, setText] = useStateWithStorage('', StorageKey)
+
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/editor">
+            <Editor
+              text={text}
+              setText={setText}
+            />
+          </Route>
+          <Route exact path="/history">
+            <History
+              setText={setText}
+            />
+          </Route>
+          <Redirect to="/editor" path="*" />
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+render(<Main />, document.getElementById('app'))
